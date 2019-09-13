f30e5aac5531fc559bf872fdf7d8f7aa99502d7a
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2019-09-13 12:21:26 +0000

    Create tomcat.bat

---
+++ tomcat.bat
@@ -0,0 +1,2 @@
+del "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\cangkitsolutions.war"
+xcopy "C:\Program Files (x86)\Jenkins\workspace\madhuapp_master\target\cangkitsolutions.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps"
