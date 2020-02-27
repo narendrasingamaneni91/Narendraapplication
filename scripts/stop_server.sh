3e02f2381bf9fb974fa7fc5667f909bd0a94ed0b
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-02-27 19:20:51 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,6 +2,5 @@
 #!/bin/bash
 isExistApp=`pgrep java`
 if [[ -n  $isExistApp ]]; then
-   cd apache-tomcat-9.0.20/bin
-sh /root/apache-tomcat-9.0.20/bin/shutdown.sh
+   sh /root/apache-tomcat-9.0.20/bin/shutdown.sh
 fi
