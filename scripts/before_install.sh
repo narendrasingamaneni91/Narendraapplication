e6317f338cf7ad061bac66daef9cf12623827da1
narendrasingamaneni91 <narendrasingamaneni91@gmail.com>
2020-06-18 18:06:50 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #rm -rf /usr/share/tomcat/webapps/cangkitsolutions.war*
-rm -rf /root/apache-tomcat-9.0.34/webapps/cangkitsolutions.war*
+rm -rf /root/apache-tomcat-8.5.56/webapps/cangkitsolutions.war*
 
